bb1193aaf26eae82b17fb5e693d1ae3d15fe1616
Angel Favila <a21490074@itmexicali.edu.mx>
2025-03-19 17:50:54 -0700

    Carpeta de Widgets y Clase para el dpad

--- poquidax/lib/view/shared/home_view.dart
+++ poquidax/lib/view/shared/home_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../../viewmodel/home_viewModel.dart';
+import 'package:pokedax/view/widgets/dpad.dart';
 
 // ignore: must_be_immutable
 class HomeView extends StatelessWidget {
@@ -17,6 +18,31 @@ class HomeView extends StatelessWidget {
     _viewModel.onBackButton = onBackButton;
   }
 
+  void _handleUp() {
+    print("Arriba");
+    // Lógica para mover hacia arriba
+  }
+
+  void _handleDown() {
+    print("Abajo");
+    // Lógica para mover hacia abajo
+  }
+
+  void _handleLeft() {
+    print("Izquierda");
+    // Lógica para mover hacia la izquierda
+  }
+
+  void _handleRight() {
+    print("Derecha");
+    // Lógica para mover hacia la derecha
+  }
+
+  void _handleCenter() {
+    print("Centro");
+    // Lógica para el botón central
+  }
+
   @override
   Widget build(BuildContext context) {
     return SizedBox(
@@ -25,8 +51,7 @@ class HomeView extends StatelessWidget {
       child: LayoutBuilder(
         builder: (context, constraints) {
           double sidePadding = constraints.maxWidth * 0.15;
-          double topPadding =
-              constraints.maxHeight * 0.05; // Subtract top padding
+          double topPadding = constraints.maxHeight * 0.05;
 
           return SizedBox(
             child: Padding(
@@ -35,10 +60,23 @@ class HomeView extends StatelessWidget {
                 top: topPadding,
                 right: sidePadding,
               ),
-              child: PokeScreenFrame.pokedexScreenFrame(
-                screenContent: screenContent,
-                screenSize: Size(screenSize.width, screenSize.height * 0.4),
-                viewModel: _viewModel,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  PokeScreenFrame.pokedexScreenFrame(
+                    screenContent: screenContent,
+                    screenSize: Size(screenSize.width, screenSize.height * 0.4),
+                    viewModel: _viewModel,
+                  ),
+                  // Cruceta (DPad)
+                  DPad(
+                    onUp: _handleUp,
+                    onDown: _handleDown,
+                    onLeft: _handleLeft,
+                    onRight: _handleRight,
+                    onCenter: _handleCenter,
+                  ),
+                ],
               ),
             ),
           );
@@ -48,104 +86,9 @@ class HomeView extends StatelessWidget {
   }
 }
 
-class PokeScreenFrame extends StatelessWidget {
-  final Size screenSize;
-  final Widget screenContent;
-  final HomeViewModel viewModel;
-
-  PokeScreenFrame.pokedexScreenFrame({
-    super.key,
-    required this.screenContent,
-    required this.screenSize,
-    required this.viewModel,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      width: screenSize.width,
-      height: screenSize.height,
-      child: Column(
-        children: [
-          Stack(
-            alignment: Alignment.topLeft,
-            children: [
-              CustomPaint(size: screenSize, painter: CutCornerPainter()),
-              pokedexScreen(screenContent, screenSize),
-            ],
-          ),
-          ElevatedButton(
-            onPressed: () {
-              viewModel.handleBackButton();
-            },
-            child: Text('Back'),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class CutCornerPainter extends CustomPainter {
-  @override
-  void paint(Canvas canvas, Size size) {
-    var paint =
-        Paint()
-          ..color = Color(0xFFDEDEDE) // Background color
-          ..style = PaintingStyle.fill;
-
-    var path = Path();
-    double cutSize = size.width * 0.15;
-
-    path.moveTo(0, 0);
-    path.lineTo(size.width, 0);
-    path.lineTo(size.width, size.height);
-    path.lineTo(cutSize, size.height);
-    path.lineTo(0, size.height - cutSize);
-    path.close();
-
-    canvas.drawPath(path, paint);
-
-    var circlePaint =
-        Paint()
-          ..color = Color(0xFFDB082D) // Circle color
-          ..style = PaintingStyle.fill;
-
-    double radius = size.width * 0.02;
-
-    canvas.drawCircle(
-      Offset(size.width / 2 - radius * 2, radius * 4),
-      radius,
-      circlePaint,
-    );
-    canvas.drawCircle(
-      Offset(size.width / 2 + radius * 2, radius * 4),
-      radius,
-      circlePaint,
-    );
-    canvas.drawCircle(
-      Offset(size.width * 0.16 - radius * 2, size.height * 0.85),
-      radius,
-      circlePaint,
-    );
-  }
-
-  @override
-  bool shouldRepaint(CustomPainter oldDelegate) => false;
-}
-
-Widget pokedexScreen(Widget screenContent, Size size) {
-  return SizedBox(
-    width: size.width,
-    height: size.height,
-    child: Padding(
-      padding: EdgeInsets.only(
-        left: size.width * 0.10,
-        top: size.height * 0.15,
-        right: size.width * 0.10,
-        bottom: size.height * 0.21,
-      ),
-      child: screenContent,
-    ),
-  );
+class PokeScreenFrame {
+  static pokedexScreenFrame(
+      {required Widget screenContent,
+      required Size screenSize,
+      required HomeViewModel viewModel}) {}
 }
