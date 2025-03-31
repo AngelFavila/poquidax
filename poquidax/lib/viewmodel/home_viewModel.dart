b5472d3a284be4a78f044233f9c46e1ae05c0977
dogAteTaco <eriml94@gmail.com>
2025-03-30 18:07:54 -0700

    Interface para VW de Pokedex

--- poquidax/lib/viewmodel/home_viewModel.dart
+++ poquidax/lib/viewmodel/home_viewModel.dart
@@ -1,11 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:pokedax/services/navigation_service.dart';
+import 'package:pokedax/viewmodel/pokedex_vm_interface.dart';
 
-class HomeViewModel extends ChangeNotifier {
+class HomeViewModel extends ChangeNotifier implements PokedexVmInterface{
 
   HomeViewModel();
 
+  @override
   void onBackButton() {
     NavigationService.go('/login');
+  }
+  
+  @override
+  void onAcceptButton() {
+    // TODO: implement onAcceptButton
+  }
+  
+  @override
+  void onDPadDown() {
+    // TODO: implement onDPadDown
+  }
+  
+  @override
+  void onDPadLeft() {
+    // TODO: implement onDPadLeft
+  }
+  
+  @override
+  void onDPadRight() {
+    // TODO: implement onDPadRight
+  }
+  
+  @override
+  void onDPadUp() {
+    // TODO: implement onDPadUp
   }
 }
---
+++ poquidax/lib/viewmodel/pokedex_vm_interface.dart
@@ -0,0 +1,13 @@
+abstract class PokedexVmInterface {
+  void onBackButton();
+
+  void onAcceptButton();
+
+  void onDPadUp();
+
+  void onDPadDown();
+
+  void onDPadLeft();
+
+  void onDPadRight();
+}
